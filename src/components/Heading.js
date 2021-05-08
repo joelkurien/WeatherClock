b02890b21aaa45605cba3429cfc0753af5e4d611
Joelkurien <joelkurien7@gmail.com>
2021-05-09 00:11:07 +0530

    App reactjs frontend completed

---
+++ src/components/Heading.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Heading(){
+  return (
+    <div className="brandName" >
+      <img src="images/new_weatherlogo.png" className="brandIcon" style={{padding: "0 20px 0 20px"}} alt="Website Background"/>
+      <p className="appName">WeatherClock</p>
+    </div>
+    );
+}
+
+export default Heading;
